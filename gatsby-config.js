dff5a4ca7aec2bb19abcb09abd2b07ec1465673f
SamirKhan21860 <msamirkhan561@gmail.com>
2024-12-29 12:13:00 +0500

    intalling dependencies and packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Velvori`,
+    description: `Get your comfort back with our products. Your Comfort, Our Aim.`,
+    author: `@velvori`,
+    siteUrl: `https://samirkhan21860.github.io/velvori`,
   },
   plugins: [
     'gatsby-plugin-postcss',
